7017a9603fcfbb8fe24c82351ccb07dcc84cef8d
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-09 00:59:54 +0300

    Add the all-quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllQuotesComponent
   ],
   imports: [
     BrowserModule,
