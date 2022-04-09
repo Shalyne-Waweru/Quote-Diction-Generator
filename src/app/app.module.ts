0255bbf8cf13a7d5aacb49c62fc6a12c3309796f
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-09 21:36:30 +0300

    Create a quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllQuotesComponent } from './all-quotes/all-quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllQuotesComponent
+    AllQuotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
