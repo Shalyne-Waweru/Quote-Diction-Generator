5a1e0691eb8d2bb1522003418144f6d72832d7e1
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-04-10 19:31:50 +0300

    Add the highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AllQuotesComponent } from './all-quotes/all-quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DateCountPipe } from './date-count.pipe';
     AllQuotesComponent,
     QuoteFormComponent,
     QuoteDetailsComponent,
-    DateCountPipe
+    DateCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
